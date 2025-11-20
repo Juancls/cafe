4be7a61a4dedde408b0ea48b98445bd9c7b1f9ea
Juancls <132762542+Juancls@users.noreply.github.com>
2025-11-19 21:37:27 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 //Encontra TODOS os botões de "abrir"
 const todosOsBotoes = document.querySelectorAll('.abrir');
-
 //  Adiciona um "escutador" de clique em CADA um deles
 todosOsBotoes.forEach(botao => {
     
@@ -14,6 +13,8 @@ todosOsBotoes.forEach(botao => {
     });
 
 });
+
+    
 
 // A função agora RECEBE o card que ela deve processar
 function criarDialog(card) {
@@ -46,14 +47,32 @@ function criarDialog(card) {
 
     const contagem = document.createElement('input');
     contagem.type = 'number';
-    contagem.setAttribute("value", "1")
+    contagem.setAttribute("value", "1");
+/*     setAttribute(tipo, valor) - colocar depois  */
+
+    const retirarIng = document.createElement('input');
+    retirarIng.setAttribute('class', 'ingredientes');
+    retirarIng.setAttribute('placeholder','Observação para cozinha');
+    retirarIng.setAttribute('type', 'text');
 
     const newBtn = document.createElement('button');
     newBtn.textContent = "Salvar";
+
+    const btnFechar = document.createElement('button');
+    btnFechar.setAttribute('class', 'btnFechar')
+    btnFechar.textContent = 'X'
     
     newBtn.autofocus = true;
 
+
     newBtn.addEventListener('click', ()=>{
+        const valorDigitado = contagem.value;
+        let quant = valorDigitado;
+
+        console.log(quant)
+    })
+
+    btnFechar.addEventListener('click', ()=>{
         document.body.classList.remove('body-sem-scroll');
         newD.close();
         newD.remove();
@@ -63,9 +82,34 @@ function criarDialog(card) {
     newD.appendChild(newTitle);
     newD.appendChild(newP);
     newD.appendChild(contagem);
+    newD.appendChild(retirarIng);
     newD.appendChild(newBtn);
+    newD.appendChild(btnFechar);
 
     document.body.appendChild(newD);
     document.body.classList.add('body-sem-scroll');
     newD.showModal();
 }   
+
+// função botão carrinho
+    const carrinho = document.querySelector('.btncarrinho');
+
+    carrinho.addEventListener('click',()=>{
+        criarBCarrinho();
+    })
+
+    function criarBCarrinho(){
+        const itemSelecionado = document.createElement('dialog');
+
+        const buttonSair = document.createElement('button');
+        buttonSair.textContent = "sair";
+
+        buttonSair.addEventListener('click',()=>{
+            itemSelecionado.close();
+            itemSelecionado.remove();
+        })
+
+        itemSelecionado.appendChild(buttonSair);
+        document.body.appendChild(itemSelecionado);
+        itemSelecionado.showModal();
+    }
